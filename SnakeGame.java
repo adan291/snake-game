35d436809a97cf6c8c1a29aeff430ecd2337581e
adan291 <adanx05@gmail.com>
2016-04-07 12:33:33 +0200

    Metodo para dar movimiento a la serpiente

--- SnakeGame.java
+++ SnakeGame.java
@@ -39,117 +39,17 @@ public class SnakeGame
         snake.dibujar(lienzo);
 
     }
-    
-     /**
+
+    /**
      * Metodo para borrar la pantalla
      */
     public void erase(){
         lienzo.erase();
     }
     
-    /**
-     * Metodo que crea una galleta de forma aleatoria, en posiciones validas
-     */
-    public Galleta creaGalleta()
-    {
-        Galleta galleta = null;
-        // Genera coordenadas
-        int coordX = 0;
-        int coordY = 0;
-        int lado;
-        boolean validas = true;
-        boolean seleccionadas = false;
-        // Comprueba si las coordenadas las tiene la serpiente u otra galleta. Usa un indice para darle
-        // una salida posible y evitar bucles infinitos si se queda sin coordenadas validas
-        int indice = 0;
-        while(!seleccionadas && indice < 10)
-        {
-            validas = true;
-            // Genera coordenadas para X e Y
-            int tempX = (int)lienzo.getSize().getWidth() - Snake.SIZE - 1;
-            int tempY = (int)lienzo.getSize().getHeight() - Snake.SIZE - 1;
-            coordX = rand.nextInt(tempX) + Snake.SIZE;
-            coordY = rand.nextInt(tempY) + Snake.SIZE;
-            // Deben ser multiplos del size de los segmentos, para que la serpiente pueda comerlas
-            coordX = coordX - (coordX%Snake.SIZE);
-            coordY = coordY - (coordY%Snake.SIZE);
-            // Ahora comprueba que no se genere en ninguna posicion de la serpiente, ni en ninguna
-            // posicion de las galletas
-            int index = 0;
-            // Bucle para las galletas
-            while(index < galletas.size() && (validas))
-            {
-                Galleta temp = galletas.get(index);
-                if((temp.getXPos() == coordX) && (temp.getYPos() == coordY))
-                {
-                    validas = false;
-                }
-                index++;
-            }
-            // Comprueba la serpiente
-            index = 0;
-            if(serpiente != null && validas)
-            {
-                int indiSerp = 0;
-                // Recorre todas las serpientes
-                while(indiSerp < serpiente.size() && (validas))
-                {
-                    // Recorre cada segmento de la serpiente, y comprueba los puntos inicial y final
-                    ArrayList<Segment> segmentos = new ArrayList<Segment>(serpiente.get(indiSerp).getSerpiente());
-                    while(index < serpiente.size() && (validas))
-                    {
-                        Segment segment = segmentos.get(index);
-                        if(((segment.getPosicionX() == coordX) && (segment.getPosicionY() == coordY)) ||
-                        ((segment.getPosicionFinalX() == coordX) && (segment.getPosicionFinalY() == coordY)))
-                        {
-                            validas = false;
-                        }
-                        index++;
-                    }
-                    indiSerp++;
-                }
-            }
-            indice++;
-            seleccionadas = validas;
-        }
-        if(seleccionadas)
-        {
-            lado = rand.nextInt(Snake.SIZE/2) + 1;
-            galleta = new Galleta(coordX, coordY, lado, lienzo);
-        }
-        return galleta;
-    }
     
-     /**
-     * Metodo que comprueba si la galleta es comida por la serpiente
-     */
-    private Galleta compruebaGalletas(Snake serp)
-    {
-        Galleta galletaComida = null;
-        if((snake != null) && (galletas.size() > 0))
-        {
-            // Toma el ultimo segmento de la serpiente
-            ArrayList<Segment> segmentos = new ArrayList<Segment>(serp.getSerpiente());
-            Segment segment = segmentos.get(serpiente.size() - 1);
-            int index = 0;
-            // Recorre la arraylist de galletas para ver si concide alguna coordenada
-            while(index < galletas.size() && (galletaComida == null))
-            {
-                Galleta galleta = galletas.get(index);
-                if(((segment.getPosicionX() == galleta.getXPos()) && (segment.getPosicionY() == galleta.getYPos())) ||
-                ((segment.getPosicionFinalX() == galleta.getXPos()) && (segment.getPosicionFinalY()) == galleta.getYPos()))
-                {
-                    // Guarda la galleta que se come y la borra
-                    galletaComida = galleta;
-                    galletas.remove(galleta);
-                }
-                index++;
-            }
-        }
-        return galletaComida;
-    }
 
-     /**
+    /**
      * Metodo para dar movimiento a la serpiente
      */
     public boolean animateSnake()
@@ -187,63 +87,6 @@ public class SnakeGame
         }
         return moverse;
     }
-
-    /**
-     * Metodo que pinta las galletas por pantalla
-     */
-    private void pintaGalletas()
-    {
-        for(int i = 0; i < galletas.size(); i++)
-        {
-            galletas.get(i).drawGalleta();
-        }
-    }
-
-    /**
-     * Muestra el mensaje de game over
-     */
-    public void gameOver()
-    {
-
-        lienzo.setForegroundColor(Color.RED);
-        int posX = (int)(lienzo.getSize().getWidth())/2 - 30;
-        int posY = (int)(lienzo.getSize().getHeight())/2 - 10;
-        lienzo.drawString("GAME OVER",posX, posY);
-    }
-     /**
-     * Metodo que inicia el juego
-     */
-    public void startGame()
-    {
-        
-        // Genera las coordenadas para la serpiente, seran siempre coordenadas validas
-        int tempX = (int)lienzo.getSize().getWidth() - Snake.SIZE*2 - 1;
-        int tempY = (int)lienzo.getSize().getHeight() - Snake.SIZE*2 - 1;
-        int coordX = rand.nextInt(tempX) + Snake.SIZE*2;
-        int coordY = rand.nextInt(tempY) + Snake.SIZE*2;
-        // Deben ser multiplos del size de los segmentos
-        coordX = coordX - (coordX%Snake.SIZE);
-        coordY = coordY - (coordY%Snake.SIZE);
-        // Crea las serpientes indicadas
-       drawSnake();
-        // Crea un numero de galletas aleatorias
-        int numeroGalletas = rand.nextInt(80) + (10*serpiente.size());
-        Galleta galleta;
-        for(int i = 0; i < numeroGalletas; i++)
-        {
-            galleta = creaGalleta();
-            if(galleta != null)
-            {
-                galletas.add(galleta);
-            }
-        }
-        // Comienza a moverse la serpiente
-        boolean juega = true;
-        while(juega)
-        {
-            juega = animateSnake();
-        }
-        gameOver();
-    }
+    
 
 }
